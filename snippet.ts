81166943f2fb317bcde7cc9f19b00857863fd46c
KaustubhK711 <43452300+KaustubhK711@users.noreply.github.com>
2024-08-08 11:33:42 +0530

    Update snippet.ts

--- snippet.ts
+++ snippet.ts
@@ -1,10 +1,25 @@
+import { SpanKind } from '@opentelemetry/api';
+import { trace, propagation, context } from '@opentelemetry/api';
+
 const tracer = trace.getTracer('your-service-name');
-tracer.startActiveSpan(
-  `${this.constructor.name}.your-method-name`,
-  { kind: SpanKind.SERVER },
-  async (span) => {
-    // Add your logic here
-    //
-    span.end();
-  }
-)
+const extracedContext = propagation.extract(
+  context.active(),
+  webhookOrderRequest?.trace_context,
+);
+context.with(extracedContext, () => {
+  tracer.startActiveSpan(
+    `${this.constructor.name}.you-method-name`,
+    {
+      kind: SpanKind.SERVER,
+      attributes: {
+        'controller.name': this.constructor.name,
+        'method.name': 'your-method-name',
+      },
+    },
+    async (span) => {
+      // Add your method logic here
+      // It will trace everything that is here
+      span.end();
+    }
+  )
+})
